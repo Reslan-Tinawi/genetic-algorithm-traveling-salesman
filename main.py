de58ed4e08ce8cda6faef175eeb41a5ff9113827
reslan <reslan3121@gmail.com>
2020-03-22 18:52:42 +0200

    test commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from GeneticAlgorithm import *
 
 def main():
 
-    ga = GeneticAlgorithm(number_of_generations=500, population_size=200, chromosome_length=20,
+    ga = GeneticAlgorithm(number_of_generations=500, population_size=250, chromosome_length=20,
                             mutation_rate=0.2, chromosome_csv_path='data/data 20 (circle).csv')
     ga.solve()
     ga.animate_solutions()
