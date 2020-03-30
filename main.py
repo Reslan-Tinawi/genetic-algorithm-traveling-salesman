13092ad648083c89f5a9749c99253d1878b51761
max <max10541@hotmail.com>
2020-03-30 15:42:53 +0300

    remove the animation function and print the best chromosome instead

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from GeneticAlgorithm import *
 
-def main():
 
+def main():
     ga = GeneticAlgorithm(number_of_generations=500, population_size=250, chromosome_length=20,
                           mutation_rate=0.005, chromosome_csv_path='data/data 20 (circle).csv')
     ga.solve()
-    ga.animate_solutions()
+    print(ga.best_chromosome)
+
 
 if __name__ == "__main__":
     main()
